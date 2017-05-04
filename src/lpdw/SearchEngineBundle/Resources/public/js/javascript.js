699a7bbeca6e9ed3dab5d69ab236ee67a4f5901b
Maxime SAINJON <maxsainjon@hotmail.fr>
2017-05-04 10:10:04 +0200

    test feature

--- src/lpdw/SearchEngineBundle/Resources/public/js/javascript.js
+++ src/lpdw/SearchEngineBundle/Resources/public/js/javascript.js
@@ -1,1 +1,138 @@
+$( document ).ready(function() {
+  type = $("#lpdw_searchenginebundle_feature_type").val();
+  i = 1;
 
+  if(type=="choix déroulant"){
+    $('#addInput').html(
+      '<label for="select_'+i+'">Valeur '+i+'</label>'+
+      '<input type="text" name="input_select_'+i+'" id="select_'+i+'"><br>'+
+      '<button id="add_select" type="button" onClick="addSelect()">+</button><br>'
+    );
+  }
+  else if (type=="checkbox") {
+    $('#addInput').html(
+      '<label for="checkbox_'+i+'">Valeur '+i+'</label>'+
+      '<input type="text" name="input_checkbox_'+i+'" id="checkbox_'+i+'"><br>'+
+      '<label for="checkbox_comment_'+i+'">Commentaire '+i+'</label>'+
+      '<textarea name="comment_checkbox_'+i+'" id="checkbox_comment_'+i+'"></textarea><br>'+
+      '<br><label for="checkbox_image_'+i+'">Image '+i+'</label>'+
+      '<input type="text" name="image_checkbox_'+i+'" id="checkbox_image_'+i+'"><br>'+
+      '<button id="add_checkbox" type="button" onClick="addCheckbox()">+</button><br>'
+    );
+  }
+  else if (type=="radio") {
+    $('#addInput').html(
+      '<label for="radio_1">Valeur 1</label>'+
+      '<input type="text" name="input_radio" id="radio_1"><br>'+
+      '<label for="radio_2">Valeur 2</label>'+
+      '<input type="text" name="input_radio" id="radio_2">'
+    );
+  }
+  else if (type=="text") {
+    $('#addInput').html(
+      '<label for="text">Valeur</label>'+
+      '<input type="text" name="input_text" id="text">'
+    );
+  }
+  else if (type=="number") {
+    $('#addInput').html(
+      '<label for="number">Valeur</label>'+
+      '<input type="number" name="input_number" id="number">'
+    );
+  }
+  else if (type=="range") {
+    $('#addInput').html(
+      '<label for="min">min</label>'+
+      '<input type="text" name="input_min" id="min">'+
+      ' - '
+      +'<label for="max">max</label>'+
+      '<input type="text" name="input_max" id="max">'
+    );
+  }
+  else if (type=="boolean") {
+    $('#addInput').html(
+      '<label for="boolean">Valeur</label>'+
+      '<input type="text" name="input_boolean" id="boolean">'
+    );
+  }
+
+
+  $("#lpdw_searchenginebundle_feature_type").change(function(){
+    i=1;
+
+    if($(this).val()=="choix déroulant"){
+      $('#addInput').html(
+        '<label for="select_'+i+'">Valeur '+i+'</label>'+
+        '<input type="text" name="input_select_'+i+'" id="select_'+i+'"><br>'+
+        '<button id="add_select" type="button" onClick="addSelect()">+</button><br>'
+      );
+    }
+    else if($(this).val()=="checkbox"){
+      $('#addInput').html(
+        '<label for="checkbox_'+i+'">Valeur '+i+'</label>'+
+        '<input type="text" name="input_checkbox_'+i+'" id="checkbox_'+i+'"><br>'+
+        '<label for="checkbox_comment_'+i+'">Commentaire '+i+'</label>'+
+        '<textarea name="comment_checkbox_'+i+'" id="checkbox_comment_'+i+'"></textarea><br>'+
+        '<br><label for="checkbox_image_'+i+'">Image '+i+'</label>'+
+        '<input type="text" name="image_checkbox_'+i+'" id="checkbox_image_'+i+'"><br>'+
+        '<button id="add_checkbox" type="button" onClick="addCheckbox()">+</button><br>'
+      );
+    }
+    else if ($(this).val()=="radio") {
+      $('#addInput').html(
+        '<label for="radio_1">Valeur 1</label>'+
+        '<input type="text" name="input_radio" id="radio_1"><br>'+
+        '<label for="radio_2">Valeur 2</label>'+
+        '<input type="text" name="input_radio" id="radio_2">'
+      );
+    }
+    else if ($(this).val()=="text") {
+      $('#addInput').html(
+        '<label for="text">Valeur</label>'+
+        '<input type="text" name="input_text" id="text">'
+      );
+    }
+    else if ($(this).val()=="number") {
+      $('#addInput').html(
+        '<label for="number">Valeur</label>'+
+        '<input type="number" name="input_number" id="number">'
+      );
+    }
+    else if ($(this).val()=="range") {
+      $('#addInput').html(
+        '<label for="min">min</label>'+
+        '<input type="text" name="input_min" id="min">'+
+        ' - '
+        +'<label for="max">max</label>'+
+        '<input type="text" name="input_max" id="max">'
+      );
+    }
+    else if ($(this).val()=="boolean") {
+      $('#addInput').html(
+        '<label for="boolean">Valeur</label>'+
+        '<input type="text" name="input_boolean" id="boolean">'
+      );
+    }
+  })
+
+});
+
+function addSelect(){
+  i++;
+  $('#select_'+(i-1)+'').after(
+    '<br><label for="select_'+i+'">Valeur '+i+'</label>'+
+    '<input type="text" name="input_select_'+i+'" id="select_'+i+'">'
+  );
+}
+
+function addCheckbox(){
+  i++;
+  $('#checkbox_image_'+(i-1)+'').after(
+    '<br><label for="checkbox_'+i+'">Valeur '+i+'</label>'+
+    '<input type="text" name="input_checkbox_'+i+'" id="checkbox_'+i+'">'+
+    '<br><label for="checkbox_comment_'+i+'">Commentaire '+i+'</label>'+
+    '<textarea name="comment_checkbox_'+i+'" id="checkbox_comment_'+i+'"></textarea>'+
+    '<br><label for="checkbox_image_'+i+'">Image '+i+'</label>'+
+    '<input type="text" name="image_checkbox_'+i+'" id="checkbox_image_'+i+'">'
+  );
+}
